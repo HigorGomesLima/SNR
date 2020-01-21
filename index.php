62293e820bcde9598a3d85c5d276a2d95ac894dd
HigorGomesLima <37384951+HigorGomesLima@users.noreply.github.com>
2020-01-21 19:22:16 -0300

    Protocolo AJAX

--- index.php
+++ index.php
@@ -1,71 +1,141 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Sistema de Notas</title>
-
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-<header>
-            
-            <div id="top">
-                <img src="img/marca.png">
-                <h2>ESCOLA SESI CAMPINAS</h2>
-                <h4>PLANILHA DE NOTAS DE RECUPERAÇÃO</h4>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Formulário de usuário</title>
+        <script src="js/jquery-1.8.2.min.js" type="text/javascript" ></script>
+        <style>
+               label{
+                display: block;
+            }
+            .window{
+                display:none;
+                width:200px;
+                height:300px;
+                position:absolute;
+                left:0;
+                top:0;
+                background:#FFF;
+                z-index:9900;
+                padding:10px;
+                border-radius:10px;
+            }
+            #mascara{
+                display:none;
+                position:absolute;
+                left:0;
+                top:0;
+                z-index:9000;
+                background-color:#000;
+            }
+            .fechar{display:block; text-align:right;}
+        </style>
+    </head>
+    <body>
+        <a href="#janela1" rel="modal">Novo Usuario</a>
+<!--        Tabela de exibição dos dados-->
+        <div id="table">
+            <table  border="1px" cellpadding="5px" cellspacing="0">
+                <tr>
+                    <td>Id</td>
+                    <td>Nome</td>
+                    <td>Email</td>
+                    <td>Senha</td>
+                </tr>
+                <?php
+//precisamos chamar esta página para realizarmos as queries com o banco
+                include 'conexao.php'; 
+// Select que traz todos os usuario cadastrados no banco de dados
+                $select = "SELECT * FROM USUARIO";
+                $result = mysql_query($select); //resultado do select
+//Enquanto existir usuários no banco ele insere uma nova linha e exibe os dados
+                while ($row = mysql_fetch_array($result)) { 
+                    $id = $row['ID_USUARIO'];
+                    $nome = $row['NOME'];
+                    $email = $row['EMAIL'];
+                    $senha = $row['SENHA'];
+ 
+                    echo"   <tr>
+                <td>$id</td>
+                <td>$nome</td>
+                <td>$email</td>
+                <td>$senha</td>
+            </tr>";
+                }
+                ?>
+            </table>
+             
+<!--            Modal que é aberto ao clicar novo usuario-->
+             
+            <div class="window" id="janela1">
+                <a href="#" class="fechar">X Fechar</a>
+                <h4>Cadastro de usuario</h4>
+                <form id="cadUsuario" action="" method="post">
+                    <label>Nome:</label><input type="text" name="nome" id="nome" />
+                    <label>Email:</label><input type="text" name="email" id="email" />
+                    <label>Senha:</label> <input type="text" name="senha" id="senha" />
+                    <br/><br/>
+                    <input type="button" value="Salvar" id="salvar" />
+                </form>
             </div>
-
-        </header>
-        <main>
-    <form action="listar.php" method="POST">
-    <label>Nome Docente
-    <input type='text' class="form-control" name='docente'></label>
-    <label>Selecione a disciplina
-    <select type='submit' class="custom-select" name='disciplina'>
-            <option value='Artes'>Arte</option>
-            <option value='Ciencias'>Ciências</option>
-            <option value='Ed. Fisica'>Educação Física</option>
-            <option value='Ens. Religioso'>Ensino Religioso</option>
-            <option value='Espanhol'>Espanhol</option>
-            <option value='Filosofia'>Filosofia</option>
-            <option value='Geografia'>Geografia</option>
-            <option value='Geometria'>Geometria</option>
-            <option value='Historia'>História</option>
-            <option value='Ingles'>Inglês</option>
-            <option value='Portugues'>Língua Portuguesa</option>
-            <option value='Leitura'>Leitura e Escrita</option>
-            <option value='Matematica'>Matemática</option>
-            <option value='Matemática Aplicada'>Matemática Aplicada</option>
-            <option value='Redacao'>Redação</option>
-            <option value='Robotica'>Robótica</option>
-            <option value='Sociologia'>Sociologia</option>
-        </select>
-    </label>
-    <label>Selecione a turma
-    <select type='submit' name = "turma" class="custom-select" id="inputGroupSelect01">
-        <option value="6-001">6º 1</option>
-        <option value="6-002">6º 2</option>
-        <option value="6-003">6º 3</option>
-        <option value="6-004">6º 4</option>
-        <option value="7-001">7º 1</option>
-        <option value="7-002">7º 2</option>
-        <option value="7-003">7º 3</option>
-        <option value="7-004">7º 4</option>
-        <option value="8-001">8º 1</option>
-        <option value="8-002">8º 2</option>
-        <option value="8-003">8º 3</option>
-        <option value="8-004">8º 4</option>
-        <option value="9-001">9º 1</option>
-        <option value="9-002">9º 2</option>
-        <option value="9-003">9º 3</option>
-        <option value="9-004">9º 4</option>
-        <option value="9-005">9º 5</option>
-    </select>
-    </label>
-    <button class='btn btn-primary' type="submit">Carregar</button>
-    </form>
-    </main>
-    <script src="js/jquery.js"></script>
-</body>
+            <div id="mascara"></div>
+        </div>
+    </body>
 </html>
+ 
+<script type="text/javascript" language="javascript">
+    $(document).ready(function() {
+        /// Quando usuário clicar em salvar será feito todos os passo abaixo
+        $('#salvar').click(function() {
+ 
+            var dados = $('#cadUsuario').serialize();
+ 
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                url: 'salvar.php',
+                async: true,
+                data: dados,
+                success: function(response) {
+                    location.reload();
+                }
+            });
+ 
+            return false;
+        });
+ 
+//// aqui é o script para abrir o nosso pequeno modal
+ 
+        $("a[rel=modal]").click(function(ev) {
+            ev.preventDefault();
+ 
+            var id = $(this).attr("href");
+ 
+            var alturaTela = $(document).height();
+            var larguraTela = $(window).width();
+ 
+            //colocando o fundo preto
+            $('#mascara').css({'width': larguraTela, 'height': alturaTela});
+            $('#mascara').fadeIn(1000);
+            $('#mascara').fadeTo("slow", 0.8);
+ 
+            var left = ($(window).width() / 2) - ($(id).width() / 2);
+            var top = ($(window).height() / 2) - ($(id).height() / 2);
+ 
+            $(id).css({'top': top, 'left': left});
+            $(id).show();
+        });
+ 
+        $("#mascara").click(function() {
+            $(this).hide();
+            $(".window").hide();
+        });
+ 
+        $('.fechar').click(function(ev) {
+            ev.preventDefault();
+            $("#mascara").hide();
+            $(".window").hide();
+        });
+ 
+    });
+</script>
